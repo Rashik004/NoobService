45df8044f85282de00725abf2314f9a569b004a5
Rashik Hasnat <rashikhasnat@ymail.com>
2017-03-20 21:53:43 +0600

    misc

--- NoobService/Service1.cs
+++ NoobService/Service1.cs
@@ -12,21 +12,14 @@ using CodeLibrary.Logger;
 
 namespace NoobService
 {
-    public partial class Service1:ServiceBase
+    public partial class Service1 : ServiceBase
     {
         private Logger _logger;
+
         public Service1()
         {
             InitializeComponent();
             _logger = new Logger();
-            //eventLog1 = new EventLog();
-            //if (!EventLog.SourceExists("MySource"))
-            //{
-            //    EventLog.CreateEventSource(
-            //        "MySource", "MyNewLog");
-            //}
-            //eventLog1.Source = "MySource";
-            //eventLog1.Log = "MyNewLog";
         }
 
         public void OnDebug()
@@ -34,26 +27,27 @@ namespace NoobService
             _logger = new Logger();
             OnStart(null);
         }
+
         protected override void OnStart(string[] args)
         {
-            //eventLog1.WriteEntry("In OnStart");
             System.Timers.Timer timer = new System.Timers.Timer();
             _logger.Debug("Hello world of Logging!!!!");
-            timer.Interval = 10000; // 60 seconds  
+            timer.Interval = 30000; // 60 seconds  
             timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
-            //Logger loging = new Logger();
             timer.Start();
             System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "Onstart.txt");
         }
 
         private void OnTimer(object sender, ElapsedEventArgs e)
         {
-            eventLog1.WriteEntry("Monitoring the System",EventLogEntryType.Information);
+            _logger.Debug("service timer logging");
         }
 
         protected override void OnStop()
         {
+            _logger.Fatal("service has been stopped!!!!!!");
             System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "Onstop.txt");
+            _logger.Fatal("service has been stopped again!!!!!!");
 
         }
     }
